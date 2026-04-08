89724c6c46ce817f22aec81276b175d099071cf1
stalingYZFR6 <stalinggosling@gmail.com>
2026-04-08 08:06:55 -0600

    login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,14 +1,71 @@
-import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import FormularioLogin from "../components/login/FormularioLogin";
+import { supabase } from "../database/supabaseconfig";
+
 const Login = () => {
+    const [usuario, setUsuario] = useState("");
+    const [contrasena, setContrasena] = useState("");
+    const [error, setError] = useState(null);
+    const navegar = useNavigate();
+
+    // Estilo del contenedor
+    const estiloContenedor = {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "linear-gradient(135deg, #FFDEE9, #B5FFFC)",
+        overflow: "hidden",
+        padding: "20px",
+    };
+
+    const iniciarSesion = async () => {
+        try {
+            const { data, error } = await supabase.auth.signInWithPassword({
+                email: usuario,
+                password: contrasena,
+            });
+
+            if (error) {
+                setError("Usuario o contraseña incorrectos");
+                return;
+            }
+
+            if (data.user) {
+                localStorage.setItem("usuario-supabase", usuario);
+                navegar("/");
+            }
+        } catch (err) {
+            setError("Error al conectar con el servidor");
+            console.error("Error en la solicitud:", err);
+        }
+    };
+
+    useEffect(() => {
+        const usuarioGuardado = localStorage.getItem("usuario-supabase");
+        if (usuarioGuardado) {
+            navegar("/");
+        }
+    }, [navegar]);
+
     return (
-        <Container className="mt-3">
-            <Row className="align-items-center">
-                <Col>
-                    <h2><i className="bi-house-fill me-2"></i> Login</h2>
-                </Col>
-            </Row>
-        </Container>
+        <div style={estiloContenedor}>
+            <FormularioLogin
+                usuario={usuario}
+                contrasena={contrasena}
+                error={error}
+                setUsuario={setUsuario}
+                setContrasena={setContrasena}
+                iniciarSesion={iniciarSesion}
+            />
+        </div>
     );
 };
+
 export default Login;
