1665a0bde00289ee8b242dd18cf64609b855b0cc
stalingYZFR6 <stalinggosling@gmail.com>
2026-06-10 09:17:33 -0600

    venta corregido

--- src/views/Ventas.jsx
+++ src/views/Ventas.jsx
@@ -6,7 +6,7 @@ import CuadroBusquedas from "../components/busquedas/CuadroBusquedas";
 import Paginacion from "../components/ordenamiento/Paginacion";
 import TablaVentas from "../components/ventas/TablaVentas";
 import TarjetaVenta from "../components/ventas/TarjetaVenta";
-import FormularioVenta from "../components/ventas/FormularioVenta";
+import FormularioVenta from "../components/ventas/Formularioventa"
 
 const Ventas = () => {
   const [toast, setToast] = useState({ mostrar: false, mensaje: "", tipo: "" });
